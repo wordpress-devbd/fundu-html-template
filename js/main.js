25301171102f54d5daa5a03ae3dd885824351f8f
Habibur Rahman <41167924+habibur899@users.noreply.github.com>
2022-02-15 13:56:08 +0600

    Header Done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 (function ($) {
 "use strict";
 
+// data - background
+$("[data-background]").each(function () {
+	$(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
+})
+
+$("[data-bg-color]").each(function () {
+	$(this).css("background", $(this).attr("data-bg-color"))
+})
+
 // meanmenu
 $('#mobile-menu').meanmenu({
 	meanMenuContainer: '.mobile-menu',
