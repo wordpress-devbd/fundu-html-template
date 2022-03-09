9b725d27b36cb7f104c1bb1cc01f1cb637268cf5
wordpress-devbd <86607177+wordpress-devbd@users.noreply.github.com>
2022-03-09 16:30:50 +0600

    Responsive

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     infinite: true,
-                    dots: true,
+                    dots: false,
                 },
             },
             {
